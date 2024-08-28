72f3619583fcd028371a63c61b32a98d63a74bbc
Patico-hash <bienioliseheseka@gmail.com>
2024-08-28 15:02:20 +0100

    final commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Currency Converter
+
+A simple application developed with Kotlin and Compose UI to convert to multiple currencies at once.
+
+- Kotlin
+- Compose UI
+- XML View
+- Firebase Database
+- Firebase Cloud Functions (TypeScript)
+- Hilt for Dependency Injection
---
+++ app/build.gradle.kts
@@ -0,0 +1,112 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.googleGmsGoogleServices)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.google.devtools.ksp)
+}
+
+android {
+    namespace = "org.dreamerslab.currencyconverter"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "org.dreamerslab.currencyconverter"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        debug {
+            buildConfigField(
+                type = "Boolean",
+                name = "USE_EMULATOR",
+                value = properties["USE_EMULATOR"]?.toString() ?: "false"
+            )
+        }
+        release {
+            buildConfigField(
+                type = "Boolean",
+                name = "USE_EMULATOR",
+                value = "false"
+            )
+
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+composeCompiler {
+    enableStrongSkippingMode = true
+    metricsDestination = layout.buildDirectory.dir("compose_compiler")
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.activity.compose)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    implementation(libs.androidx.core.splashscreen)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+
+    implementation(libs.lottie.compose)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.coil.compose)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.database.ktx)
+
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+}
